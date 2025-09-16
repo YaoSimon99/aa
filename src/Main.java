88e19e814e907a4fd2948e7788d50c589490c7d8
yaosimon99 <1756699170@qq.com>
2025-09-16 22:36:57 +0800

    xyh

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String a = new String("ZSD");
+        String b = new String("ZSD");
+        System.out.println(a == b);
+    }
+}
+
